da0017734b34320df8ae1eb16124253f1b58ac9e
David Van Osdol <davidvanosdol88@gmail.com>
2025-12-07 18:26:59 -0500

    Fix: Rename db.js to db.ts and add types

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,7 +1,7 @@
-import { collection, addDoc, onSnapshot } from "firebase/firestore";
+import { collection, addDoc, onSnapshot, DocumentData, QuerySnapshot } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 
-export const addItem = async (collectionName, data) => {
+export const addItem = async (collectionName: string, data: any) => {
     try {
         const docRef = await addDoc(collection(db, collectionName), data);
         return docRef.id;
@@ -11,10 +11,10 @@ export const addItem = async (collectionName, data) => {
     }
 };
 
-export const subscribeToItems = (collectionName, callback) => {
+export const subscribeToItems = (collectionName: string, callback: (items: any[]) => void) => {
     const q = collection(db, collectionName);
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        const items = [];
+    const unsubscribe = onSnapshot(q, (querySnapshot: QuerySnapshot<DocumentData>) => {
+        const items: any[] = [];
         querySnapshot.forEach((doc) => {
             items.push({ id: doc.id, ...doc.data() });
         });
